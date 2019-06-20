99ccad7a415038c65a13267d5dd372ff85250b38
NickodemusRR <nickodemus.richard@gmail.com>
2019-06-20 14:34:58 +0700

    versi 1

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for, abort
+import requests
+import joblib
+
+app = Flask(__name__)
+
+# home route
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# route untuk memasukkan data rumah yang dicari
+@app.route('/post', methods=['POST'])
+def cari():
+    usia = int(request.form['usia'])
+    kamar = int(request.form['kamar'])
+    luas = int(request.form['luas'])
+    # model diimport menggunakan package joblib
+    model = joblib.load('../17SK_Learn/modelJoblib')
+    harga = round(model.predict([[usia, kamar, luas]])[0], 2)
+    return redirect(url_for('hasil', harga=harga))
+
+# route untuk menampilkan hasil perhitungan
+@app.route('/hasil/<int:harga>')
+def hasil(harga):
+    return render_template('hasil.html', harga=harga)
+
+# route untuk menampilkan halaman error
+@app.errorhandler(404)
+def error(error):
+    return render_template('error.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
